1052514a4cd6efbe52c1f88cb32d941c34ab2e1a
Malak <malakalmajed15@gmail.com>
2026-04-01 15:53:34 +0300

    adding application user model

---
+++ WebAPI/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Models
+   
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string? ProfilePicture { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+
+        public Doctor? Doctor {  get; set; }
+        public Patient? Patient { get; set; }
+    }
+}
