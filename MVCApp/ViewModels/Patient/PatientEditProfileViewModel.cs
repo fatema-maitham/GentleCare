b051d72c3abb5804dc95d557f13bb28eff6f696b
lllmaram <maram20052@gmail.com>
2026-05-15 23:36:09 +0300

    add patient profile photo upload and default image support

--- MVCApp/ViewModels/Patient/PatientEditProfileViewModel.cs
+++ MVCApp/ViewModels/Patient/PatientEditProfileViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace MVCApp.ViewModels.Patient
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCApp.ViewModels.Patient
 {
-    public class PatientProfileViewModel
+    public class PatientEditProfileViewModel
     {
+        [Required]
+        [StringLength(100)]
         public string FullName { get; set; } = string.Empty;
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         public string CPRNumber { get; set; } = string.Empty;
@@ -12,14 +19,20 @@
 
         public DateTime DateOfBirth { get; set; }
 
+        [StringLength(10)]
         public string? BloodType { get; set; }
 
+        [StringLength(250)]
         public string? Address { get; set; }
 
+        [StringLength(100)]
         public string? EmergencyContactName { get; set; }
 
+        [StringLength(30)]
         public string? EmergencyContactPhone { get; set; }
 
-        public string? ProfilePicture { get; set; }
+        public string? CurrentProfilePicture { get; set; }
+
+        public IFormFile? ProfileImageFile { get; set; }
     }
 }
