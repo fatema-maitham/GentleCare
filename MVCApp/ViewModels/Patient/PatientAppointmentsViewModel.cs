16e5653d576c4cff7449722d8993e2553bcc4f86
lllmaram <maram20052@gmail.com>
2026-05-15 23:32:43 +0300

    add patient profile and edit profile view models

--- MVCApp/ViewModels/Patient/PatientAppointmentsViewModel.cs
+++ MVCApp/ViewModels/Patient/PatientAppointmentsViewModel.cs
@@ -3,11 +3,20 @@
     public class PatientAppointmentsViewModel
     {
         public int AppointmentId { get; set; }
+
         public DateTime AppointmentDate { get; set; }
+
         public string StartTime { get; set; } = string.Empty;
+
         public string EndTime { get; set; } = string.Empty;
+
         public string DoctorName { get; set; } = string.Empty;
+
         public string Status { get; set; } = string.Empty;
+
         public string? Notes { get; set; }
+
+        public bool CanCancel =>
+            Status == "Requested" || Status == "Confirmed";
     }
 }
