1699ff7f10bf3ad019f8dc57c1c7801faaaefe8d
lllmaram <maram20052@gmail.com>
2026-05-16 04:55:41 +0300

    add receptionist patient search

---
+++ MVCApp/ViewModels/Receptionist/ReceptionistPatientSearchResultViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MVCApp.ViewModels.Receptionist
+{
+    public class ReceptionistPatientSearchResultViewModel
+    {
+        public int PatientId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string CPRNumber { get; set; } = string.Empty;
+
+        public string ReferenceNumber { get; set; } = string.Empty;
+
+        public string DateOfBirth { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
