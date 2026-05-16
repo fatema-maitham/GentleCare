4772d652614bf8e5f44f92fc8fb23f7223c54851
fatema-maitham <fatemamaitham1@gmail.com>
2026-05-16 13:30:57 +0300

    create announcement view model

---
+++ MVCApp/ViewModels/ClinicManager/ClinicAnnouncementViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCApp.ViewModels.ClinicManager
+{
+    // ViewModel for Clinic Manager announcement creation.
+    // The manager can send one announcement to doctors, receptionists, patients, or all.
+    public class ClinicAnnouncementViewModel
+    {
+        [Required(ErrorMessage = "Announcement title is required.")]
+        [StringLength(120, ErrorMessage = "Title cannot be more than 120 characters.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Announcement message is required.")]
+        [StringLength(1000, ErrorMessage = "Message cannot be more than 1000 characters.")]
+        public string Message { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select who should receive this announcement.")]
+        public string SelectedAudience { get; set; } = "All";
+
+        public List<SelectListItem> AudienceOptions { get; set; } = new();
+    }
+}
